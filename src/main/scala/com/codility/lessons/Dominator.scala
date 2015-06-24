90efb65c2c29ee5f64424b227262c8866ef9f46f
Jakub Narloch <jmnarloch@gmail.com>
2015-06-24 21:19:28 +0200

    Functional solution for Dominator.scala

--- src/main/scala/com/codility/lessons/Dominator.scala
+++ src/main/scala/com/codility/lessons/Dominator.scala
@@ -44,4 +44,62 @@ object Dominator {
 
     firstIndex
   }
+
+  def funcSolution(A: Array[Int]): Int = {
+
+    if (A.length == 0) {
+      return -1
+    }
+
+    var candidate = A(0)
+    var occurrences = 1
+    var firstIndex = -1
+
+    for (ind <- 1 until A.length) {
+      if (candidate != A(ind)) {
+        occurrences -= 1
+      } else {
+        occurrences += 1
+      }
+
+      if (occurrences == 0) {
+        candidate = A(ind)
+        occurrences = 1
+      }
+    }
+
+    if(count(A, candidate) <= A.length / 2) {
+      return -1
+    }
+
+    findFirst(A, candidate)
+  }
+
+  def count(A: Array[Int], item: Int): Int = {
+    countImpl(A, item, 0, 0)
+  }
+
+  def countImpl(A: Array[Int], item: Int, ind: Int, count: Int): Int = {
+    if(ind >= A.length) {
+      count
+    } else if(A(ind) == item) {
+      countImpl(A, item, ind + 1, count + 1)
+    } else {
+      countImpl(A, item, ind + 1, count)
+    }
+  }
+
+  def findFirst(A: Array[Int], item: Int): Int = {
+      findFirstImpl(A, item, 0)
+  }
+
+  def findFirstImpl(A: Array[Int], item: Int, ind: Int): Int = {
+    if(ind >= A.length) {
+      -1
+    } else if(A(ind) == item) {
+      ind
+    } else {
+      findFirstImpl(A, item, ind + 1)
+    }
+  }
 }
--- src/test/scala/com/codility/lessons/Dominator$Test.scala
+++ src/test/scala/com/codility/lessons/Dominator$Test.scala
@@ -13,4 +13,11 @@ class Dominator$Test extends UnitSpec {
 
     List(Dominator.solution(A)) should contain oneOf (0, 2, 4, 6, 7)
   }
+
+  it should "func find leader" in {
+
+    val A = Array(3, 4, 3, 2, 3, -1, 3, 3)
+
+    List(Dominator.funcSolution(A)) should contain oneOf (0, 2, 4, 6, 7)
+  }
 }
