a5841c7dc585db6efdf95a0cf6b1f7beb3295e71
Jakub Narloch <jmnarloch@gmail.com>
2015-06-30 20:57:43 +0200

    MaxProfit.scala

---
+++ src/main/scala/com/codility/lessons/MaxProfit.scala
@@ -0,0 +1,8 @@
+package com.codility.lessons
+
+/**
+ * Created by jakubnarloch on 30.06.15.
+ */
+object MaxProfit {
+
+}
---
+++ src/test/scala/com/codility/lessons/MaxProfit$Test.scala
@@ -0,0 +1,10 @@
+package com.codility.lessons
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by jakubnarloch on 30.06.15.
+ */
+class MaxProfit$Test extends FunSuite {
+
+}
