749bd5e3407b52fdf732cd8fa7f561bda5735b8d
Jakub Narloch <jmnarloch@gmail.com>
2015-06-30 20:05:05 +0200

    Refactoring

--- src/main/scala/com/codility/lessons/Dominator.scala
+++ src/main/scala/com/codility/lessons/Dominator.scala
@@ -78,32 +78,32 @@ object Dominator {
   }
 
   def count(A: Array[Int], item: Int): Int = {
-    countImpl(A, item, 0, 0)
-  }
-
-  @tailrec
-  def countImpl(A: Array[Int], item: Int, ind: Int, count: Int): Int = {
-    if(ind >= A.length) {
-      count
-    } else if(A(ind) == item) {
-      countImpl(A, item, ind + 1, count + 1)
-    } else {
-      countImpl(A, item, ind + 1, count)
+    @tailrec
+    def count(A: Array[Int], item: Int, ind: Int, cnt: Int): Int = {
+      if(ind >= A.length) {
+        cnt
+      } else if(A(ind) == item) {
+        count(A, item, ind + 1, cnt + 1)
+      } else {
+        count(A, item, ind + 1, cnt)
+      }
     }
+
+    count(A, item, 0, 0)
   }
 
   def findFirst(A: Array[Int], item: Int): Int = {
-      findFirstImpl(A, item, 0)
-  }
-
-  @tailrec
-  def findFirstImpl(A: Array[Int], item: Int, ind: Int): Int = {
-    if(ind >= A.length) {
-      -1
-    } else if(A(ind) == item) {
-      ind
-    } else {
-      findFirstImpl(A, item, ind + 1)
+    @tailrec
+    def findFirst(A: Array[Int], item: Int, ind: Int): Int = {
+      if(ind >= A.length) {
+        -1
+      } else if(A(ind) == item) {
+        ind
+      } else {
+        findFirst(A, item, ind + 1)
+      }
     }
+
+    findFirst(A, item, 0)
   }
 }
--- src/main/scala/com/codility/lessons/EquiLeader.scala
+++ src/main/scala/com/codility/lessons/EquiLeader.scala
@@ -39,35 +39,34 @@ object EquiLeader {
   }
 
   def findCandidate(A: Array[Int]): Int = {
+    @tailrec
+    def findCandidate(A: Array[Int], ind: Int, candidate: Int, occur: Int): Int = {
+      if(ind >= A.length) {
+        candidate
+      } else if(candidate == A(ind)) {
+        findCandidate(A, ind + 1, candidate, occur + 1)
+      } else if(occur > 1) {
+        findCandidate(A, ind + 1, candidate, occur - 1)
+      } else {
+        findCandidate(A, ind + 1, A(ind), 1)
+      }
+    }
 
     findCandidate(A, 1, A(0), 1)
   }
 
-  @tailrec
-  def findCandidate(A: Array[Int], ind: Int, candidate: Int, occur: Int): Int = {
-    if(ind >= A.length) {
-      candidate
-    } else if(candidate == A(ind)) {
-      findCandidate(A, ind + 1, candidate, occur + 1)
-    } else if(occur > 1) {
-      findCandidate(A, ind + 1, candidate, occur - 1)
-    } else {
-      findCandidate(A, ind + 1, A(ind), 1)
-    }
-  }
-
   def count(A: Array[Int], item: Int): Int = {
-    count(A, item , 0, 0)
-  }
-
-  @tailrec
-  def count(A: Array[Int], item: Int, ind: Int, cnt: Int): Int = {
-    if(ind >= A.length) {
-      cnt
-    } else if(A(ind) == item) {
-      count(A, item, ind + 1, cnt + 1)
-    } else {
-      count(A, item, ind + 1, cnt)
+    @tailrec
+    def count(A: Array[Int], item: Int, ind: Int, cnt: Int): Int = {
+      if(ind >= A.length) {
+        cnt
+      } else if(A(ind) == item) {
+        count(A, item, ind + 1, cnt + 1)
+      } else {
+        count(A, item, ind + 1, cnt)
+      }
     }
+
+    count(A, item , 0, 0)
   }
 }
